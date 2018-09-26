5757cbb1abc5a21e35b7f60b16b923aa536957f1
chsu <yaostyle@gmail.com>
2018-09-26 11:00:32 -0700

    Big clean up. S02.01 starting point.

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -13,110 +13,66 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.datafrominternet;
+package com.example.android.sunshine;
 
-import android.content.Context;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.android.datafrominternet.utilities.NetworkUtils;
-
-import java.io.IOException;
-import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mSearchBoxEditText;
-    private TextView mUrlDisplayTextView;
-    private TextView mSearchResultsTextView;
-    private TextView mErrorMessageDisplay;
-    private ProgressBar mLoadingIndicator;
+    private TextView mWeatherTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_forecast);
 
-        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
-        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
-        mSearchResultsTextView = (TextView) findViewById(R.id.tv_github_search_results_json);
-        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
-        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
-    }
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-    private void makeGithubSearchQuery() {
-        String githubQuery = mSearchBoxEditText.getText().toString();
-        URL githubtSearchUrl = NetworkUtils.buildUrl(githubQuery);
-        mUrlDisplayTextView.setText(githubtSearchUrl.toString());
-        new GithubQueryTask().execute(githubtSearchUrl);
+        // TODO (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
+        /*
+         * This String array contains dummy weather data. Later in the course, we're going to get
+         * real weather data. For now, we want to get something on the screen as quickly as
+         * possible, so we'll display this dummy data.
+         */
+        String[] dummyWeatherData = {
+                "Today, May 17 - Clear - 17°C / 15°C",
+                "Tomorrow - Cloudy - 19°C / 15°C",
+                "Thursday - Rainy- 30°C / 11°C",
+                "Friday - Thunderstorms - 21°C / 9°C",
+                "Saturday - Thunderstorms - 16°C / 7°C",
+                "Sunday - Rainy - 16°C / 8°C",
+                "Monday - Partly Cloudy - 15°C / 10°C",
+                "Tue, May 24 - Meatballs - 16°C / 18°C",
+                "Wed, May 25 - Cloudy - 19°C / 15°C",
+                "Thu, May 26 - Stormy - 30°C / 11°C",
+                "Fri, May 27 - Hurricane - 21°C / 9°C",
+                "Sat, May 28 - Meteors - 16°C / 7°C",
+                "Sun, May 29 - Apocalypse - 16°C / 8°C",
+                "Mon, May 30 - Post Apocalypse - 15°C / 10°C",
+        };
 
-    }
-
-    private void showJsonDataView() {
-        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
-        mSearchResultsTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void showErrorMessage() {
-        mSearchResultsTextView.setVisibility(View.INVISIBLE);
-        mErrorMessageDisplay.setVisibility(View.VISIBLE);
-    }
-
-    public class GithubQueryTask extends AsyncTask<URL, Void, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingIndicator.setVisibility(View.VISIBLE);
+        // TODO (3) Delete the for loop that populates the TextView with dummy data
+        /*
+         * Iterate through the array and append the Strings to the TextView. The reason why we add
+         * the "\n\n\n" after the String is to give visual separation between each String in the
+         * TextView. Later, we'll learn about a better way to display lists of data.
+         */
+        for (String dummyWeatherDay : dummyWeatherData) {
+            mWeatherTextView.append(dummyWeatherDay + "\n\n\n");
         }
 
-        @Override
-        protected String doInBackground(URL... urls) {
-            URL searchUrl = urls[0];
-            String githubSearchResults = null;
-
-            try {
-                githubSearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            return githubSearchResults;
-        }
-
-        @Override
-        protected void onPostExecute(String githubSearchResults) {
-            mLoadingIndicator.setVisibility(View.INVISIBLE);
-            if (githubSearchResults != null && !githubSearchResults.equals("")) {
-                showJsonDataView();
-                mSearchResultsTextView.setText(githubSearchResults);
-            } else {
-                showErrorMessage();
-            }
-        }
+        // TODO (9) Call loadWeatherData to perform the network request to get the weather
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    // TODO (8) Create a method that will get the user's preferred location and execute your new AsyncTask and call it loadWeatherData
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int menuItemThatWasSelected = item.getItemId();
-        if (menuItemThatWasSelected == R.id.action_search) {
-            makeGithubSearchQuery();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    // TODO (5) Create a class that extends AsyncTask to perform network requests
+    // TODO (6) Override the doInBackground method to perform your network requests
+    // TODO (7) Override the onPostExecute method to display the results of the network request
 }
--- app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
+++ app/src/main/java/com/example/android/datafrominternet/utilities/NetworkUtils.java
@@ -13,55 +13,73 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.datafrominternet.utilities;
-
-import android.net.Uri;
+package com.example.android.sunshine.utilities;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 /**
- * These utilities will be used to communicate with the network.
+ * These utilities will be used to communicate with the weather servers.
  */
-public class NetworkUtils {
+public final class NetworkUtils {
 
-    final static String GITHUB_BASE_URL =
-            "https://api.github.com/search/repositories";
+    private static final String TAG = NetworkUtils.class.getSimpleName();
 
-    final static String PARAM_QUERY = "q";
+    private static final String DYNAMIC_WEATHER_URL =
+            "https://andfun-weather.udacity.com/weather";
+
+    private static final String STATIC_WEATHER_URL =
+            "https://andfun-weather.udacity.com/staticweather";
+
+    private static final String FORECAST_BASE_URL = STATIC_WEATHER_URL;
 
     /*
-     * The sort field. One of stars, forks, or updated.
-     * Default: results are sorted by best match if no field is specified.
+     * NOTE: These values only effect responses from OpenWeatherMap, NOT from the fake weather
+     * server. They are simply here to allow us to teach you how to build a URL if you were to use
+     * a real API.If you want to connect your app to OpenWeatherMap's API, feel free to! However,
+     * we are not going to show you how to do so in this course.
      */
-    final static String PARAM_SORT = "sort";
-    final static String sortBy = "stars";
+
+    /* The format we want our API to return */
+    private static final String format = "json";
+    /* The units we want our API to return */
+    private static final String units = "metric";
+    /* The number of days we want our API to return */
+    private static final int numDays = 14;
+
+    final static String QUERY_PARAM = "q";
+    final static String LAT_PARAM = "lat";
+    final static String LON_PARAM = "lon";
+    final static String FORMAT_PARAM = "mode";
+    final static String UNITS_PARAM = "units";
+    final static String DAYS_PARAM = "cnt";
 
     /**
-     * Builds the URL used to query Github.
+     * Builds the URL used to talk to the weather server using a location. This location is based
+     * on the query capabilities of the weather provider that we are using.
      *
-     * @param githubSearchQuery The keyword that will be queried for.
+     * @param locationQuery The location that will be queried for.
      * @return The URL to use to query the weather server.
      */
-    public static URL buildUrl(String githubSearchQuery) {
-        Uri buildUri = Uri.parse(GITHUB_BASE_URL).buildUpon()
-                .appendQueryParameter(PARAM_QUERY, githubSearchQuery)
-                .appendQueryParameter(PARAM_SORT, sortBy)
-                .build();
+    public static URL buildUrl(String locationQuery) {
+        // TODO (1) Fix this method to return the URL used to query Open Weather Map's API
+        return null;
+    }
 
-        URL url = null;
-
-        try {
-            url = new URL(buildUri.toString());
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-
-        return url;
+    /**
+     * Builds the URL used to talk to the weather server using latitude and longitude of a
+     * location.
+     *
+     * @param lat The latitude of the location
+     * @param lon The longitude of the location
+     * @return The Url to use to query the weather server.
+     */
+    public static URL buildUrl(Double lat, Double lon) {
+        /** This will be implemented in a future lesson **/
+        return null;
     }
 
     /**
