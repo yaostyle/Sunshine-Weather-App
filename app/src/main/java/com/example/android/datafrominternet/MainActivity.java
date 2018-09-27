2f5640d50070ba1436d7d8bd8ca6c495bed73e27
chsu <yaostyle@gmail.com>
2018-09-27 14:38:52 -0700

    Add loading indicator.

--- app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -21,6 +21,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import com.example.android.datafrominternet.data.SunshinePreferences;
 import com.example.android.datafrominternet.utilities.NetworkUtils;
@@ -31,6 +33,8 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
+    private TextView mErrorMessageDisplay;
+    private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,14 +47,28 @@ public class MainActivity extends AppCompatActivity {
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
+        mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
+        mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
         loadWeatherData();
 
     }
 
     // Create a method that will get the user's preferred location
     private void loadWeatherData() {
+        showWeatherDataView();
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
+    }
+
+    private void showWeatherDataView() {
+        mErrorMessageDisplay.setVisibility(View.INVISIBLE);
+        mWeatherTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showErrorMessage() {
+        mWeatherTextView.setVisibility(View.INVISIBLE);
+        mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
 
     // Create a class that extends AsyncTask to perform network requests
@@ -79,12 +97,24 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicator.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected void onPostExecute(String[] weatherData) {
+            mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (weatherData != null) {
+                showWeatherDataView();
+
                 for (String weatherString : weatherData) {
-                    mWeatherTextView.append((weatherString) + "\n\n\n");
+                    mWeatherTextView.append((weatherString) +  "\n\n\n");
                 }
+            } else {
+                showErrorMessage();
             }
+
         }
     }
 
